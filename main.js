a1f98b33d5b9ea4b5d9e28848ce593af41691550
prosperjohn9 <prosperjohn9@gmail.com>
2024-06-27 12:21:43 +0300

    feat: Add test cases for HashMap implementation

--- main.js
+++ main.js
@@ -16,18 +16,24 @@ test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
 
+console.log('Get apple:', test.get('apple')); // Should return 'red'
+console.log('Get banana:', test.get('banana')); // Should return 'yellow'
+
 // Overwrite some values
 test.set('apple', 'green');
 test.set('banana', 'brown');
+
+console.log('Size:', test.length()); // Should return 12
 
 // Add more nodes to exceed load factor and trigger resize
 test.set('moon', 'silver');
 
 // Test methods
+console.log('Size:', test.length()); // Should return 13
 console.log('Remove dog:', test.remove('dog')); // Should return true
 console.log('Get dog:', test.get('dog')); // Should return null
-console.log('Size:', test.length());
 console.log('Get apple:', test.get('apple')); // Should return 'green'
+console.log('Get banana:', test.get('banana')); // Should return 'brown'
 console.log('Has grape:', test.has('grape')); // Should return true
 console.log('Size after remove:', test.length());
 console.log('Keys:', test.keys());
